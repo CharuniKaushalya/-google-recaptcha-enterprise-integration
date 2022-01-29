3c33e5aec8965fef2b773a6208c7fbf5baea8c82
charuni-certscanner <c.kaushalya@certscanner.com>
2022-01-29 19:17:24 +0530

    refactor

--- resources/js/components/Captcha.vue
+++ resources/js/components/Captcha.vue
@@ -6,31 +6,16 @@
 
 <script>
 export default {
-    // data() {
-    //     dd:Object.prototype.hasOwnProperty.call(window, 'grecaptcha')
-    // },
-    //
-    // // computed : {
-    // //     dd() {
-    // //         return
-    // //     }
-    // // },
-    //
-    // watch: {
-    //   dd() {
-    //       console.log("here come 222")
-    //   }
-    // },
 
     mounted() {
         var vueRecaptchaApiLoaded = function() {
             grecaptcha.enterprise.render('g-recaptcha', {
-                'sitekey' : process.env.MIX_RECAPTCHA_SITE_KEY
+                'sitekey' : '6LcaPDgeAAAAAPvMCsGRx1PVL3n1XOR9RK8kDnek'
             });
         };
 
         window.vueRecaptchaApiLoaded =  vueRecaptchaApiLoaded
-        
+
         // const script = document.createElement('script')
         // script.id = 'recaptchascrpitid'
         // script.src = `https://www.google.com/recaptcha/enterprise.js?onload=vueRecaptchaApiLoaded&render=explicit`
